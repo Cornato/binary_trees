af1847d2bceb7a7c9bbe11c8e177d6fdc5f47dcc
Cornato <dragonroar0099@gmail.com>
2024-02-29 02:57:12 +0200

    test

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
- * binary_tree_nodes - function that returns # of nodes in a tree with children
- * @tree: tree to check
- * Return: number of nodes with children
+ * binary_tree_nodes - fchildren
+ * @tree: tree
+ * Return: 0
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
