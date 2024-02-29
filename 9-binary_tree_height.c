fd3430e4e92ea6a12dfefd164f2927395bf60b85
Cornato <dragonroar0099@gmail.com>
2024-02-29 02:30:26 +0200

    test

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,11 +13,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
     if (tree == NULL)
         return (0);
 
-    if (tree->left)
-        l = 1 + binary_tree_height(tree->left);
-
-    if (tree->right)
-        r = 1 + binary_tree_height(tree->right);
+    l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+    r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
     return ((l > r) ? l : r);
 }
