9ce7218234749654b5a4bb13a1be59fe01c3b12e
Cornato <dragonroar0099@gmail.com>
2024-02-29 02:28:33 +0200

    test

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,30 +1,23 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Function that measures the height of a binary tree
+ * binary_tree_height - tree_height
  * @tree: tree to go through
  * Return: the height
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t l = 0;
-    size_t r = 0;
+    size_t l = 0, r = 0;
 
     if (tree == NULL)
-    {
         return (0);
-    }
 
     if (tree->left)
-    {
         l = 1 + binary_tree_height(tree->left);
-    }
 
     if (tree->right)
-    {
         r = 1 + binary_tree_height(tree->right);
-    }
 
     return ((l > r) ? l : r);
 }
